1dba3d69a34c5b1b7a72fa58eddb5441dd6239dc
AntonioAcoDurand <antonio.aco.dosa@gmail.com>
2024-11-05 14:31:51 +0100

    Hola

---
+++ Programación/punteros/puntero_tut.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+	/**
+	 * Autor Antonio Óscar Aco Durand
+	 * 
+	 * > mayor
+	 * < menor 
+	 * == igual
+	 * <= menor o igual
+	 * >= mayor o igual
+	 */
+	
+struct Estudiante {
+	char nombre[50];
+	int edad;
+	float promedio;
+}
+	int main (){
+	printf("Introduce tus datos\n");
+	scanf("%c",&Estudiante.nombre);
+	scanf("%d",&Estudiante.edad);
+	scanf("%f",&Estudiante.promedio);	
+	return 0;
+	}
